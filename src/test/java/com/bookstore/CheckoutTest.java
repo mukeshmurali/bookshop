196483c5afa0a3fb9a701836485703173dbe8b99
Mukesh Murali <mukesh.murali@gamesys.co.uk>
2021-05-25 09:52:25 +0100

    refactoring setmethods in test to a single utility method

--- src/test/java/com/bookstore/CheckoutTest.java
+++ src/test/java/com/bookstore/CheckoutTest.java
@@ -1,5 +1,6 @@
 package com.bookstore;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 
 
@@ -15,14 +16,8 @@ public class CheckoutTest {
     void checkoutTwoBooksWithOneBookPublishedAfter2000() {
         //setup
         List<Item> bookList = new ArrayList<>();
-        Item book1 = new Book();
-        book1.setPrice(12.87);
-        book1.setTitle("Three Men in a Boat");
-        book1.setYear(1889);
-        Item book2 = new Book();
-        book2.setPrice(13.14);
-        book2.setTitle("The Terrible Privacy of Maxwell Sim");
-        book2.setYear(2010);
+        Item book1 = getItem(12.87, "Three Men in a Boat", 1889);
+        Item book2 = getItem(13.14, "The Terrible Privacy of Maxwell Sim", 2010);
         bookList.add(book1);
         bookList.add(book2);
         checkout = new Checkout(bookList);
@@ -30,5 +25,14 @@ public class CheckoutTest {
         Double cost=checkout.calculatePaymentAmountAfterDiscount();
         //assert
         assertEquals(24.69,cost);
+    }
+
+    @NotNull
+    private Item getItem(double price, String title, int year) {
+        Item book = new Book();
+        book.setPrice(price);
+        book.setTitle(title);
+        book.setYear(year);
+        return book;
     }
 }
