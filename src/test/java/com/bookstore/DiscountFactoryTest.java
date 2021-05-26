68ef48bd218aee6b86d84ec0e5c70655d6475d3e
Mukesh Murali <mukesh.murali@gamesys.co.uk>
2021-05-26 01:20:13 +0100

    Adding tests for Discount factory

--- src/test/java/com/bookstore/DiscountFactoryTest.java
+++ src/test/java/com/bookstore/DiscountFactoryTest.java
@@ -1,16 +1,67 @@
 package com.bookstore;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyDouble;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class DiscountFactoryTest {
 
+    @Mock
+    DiscountFactory mockDiscountFactory;
+
+    @InjectMocks
+    Item item=new Book();
+
     @Test
-    void test10PercentDiscountIfBookIsPublishedAfter2000() {
+    void createDiscount30WhenTotalIsGreaterThan30() {
         //setup
-        Discount30 order = new Discount30();
-        //act and assert
-        assertEquals(9.5, order.getDiscountedValue(10.0));
+        when(mockDiscountFactory.createDiscount(anyDouble())).thenCallRealMethod();
+        //act
+        Discount discount=mockDiscountFactory.createDiscount(40.0);
+        //assert
+        assertTrue(discount instanceof Discount30);
     }
+
+    @Test
+    void createDiscount0WhenTotalIsLesserThan30() {
+        //setup
+        when(mockDiscountFactory.createDiscount(anyDouble())).thenCallRealMethod();
+        //act
+        Discount discount=mockDiscountFactory.createDiscount(10.0);
+        //assert
+        assertTrue(discount instanceof Discount0);
+    }
+
+    @Test
+    void createDiscount0WhenYearIsGreaterThan2000() {
+        //setup
+        item.setYear(2003);
+        when(mockDiscountFactory.createDiscount(item)).thenCallRealMethod();
+        //act
+        Discount discount=mockDiscountFactory.createDiscount(item);
+        //assert
+        assertTrue(discount instanceof DiscountPublishedAfter2000);
+    }
+
+
+    @Test
+    void createDiscount0WhenYearIsLessThan2000() {
+        //setup
+        item.setYear(1889);
+        when(mockDiscountFactory.createDiscount(item)).thenCallRealMethod();
+        //act
+        Discount discount=mockDiscountFactory.createDiscount(item);
+        //assert
+        assertTrue(discount instanceof Discount0);
+    }
+
+
 }
